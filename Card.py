c62dadb06251ce13794d635bec0de551bf537f31
Andrew <Belka55tt@ysndex.ru>
2024-07-01 16:34:45 +0300

    класс карт

---
+++ Card.py
@@ -0,0 +1,37 @@
+from deck import Card
+
+
+class Card:
+    NUMBERS = list(range(10)) + list(range(1, 10))
+
+    def __init__(self, number: int):
+        self.number = number
+        if number in Card.NUMBERS:
+            self.number = number
+        else:
+            raise ValueError(f'Unexpected number {number}')
+
+    def __eq__(self, other):
+        return self.number == other.number
+
+    def __repr__(self):
+        return f'{self.number}'
+
+    def save(self):
+        return repr(self)
+
+    @staticmethod
+    def all_cards(umbers=None, numbers=None):
+        """Возвращает все карты.
+        :param colors:
+        :param numbers:
+        """
+        if numbers is None:
+            numbers = Card.NUMBERS
+            cards = []
+            for n in numbers:
+                c = Card( n)
+                cards.append(c)
+
+                # return [Card(col, n) for col in colors for n in numbers]
+        return cards
