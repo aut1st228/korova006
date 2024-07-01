be6b57fa7fe694bb93b353c86396837dcec2a7ff
Andrew <Belka55tt@ysndex.ru>
2024-07-01 16:35:18 +0300

    штрафные очки

---
+++ penalty.py
@@ -0,0 +1,15 @@
+'''расчет штрафных очков'''
+import math
+def cow_penalty_points(number):
+
+    units = math.trunc(number % 10)
+    tens = math.trunc(number / 10)
+    if units == 5 & tens == 5:
+        return 7
+    if units == tens:
+        return 5
+    if units == 0:
+        return 3
+    if units == 5:
+        return 2
+    return 1
