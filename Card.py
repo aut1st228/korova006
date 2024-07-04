39620ae509a8db007a74227830f7517025007485
Andrew <Belka55tt@ysndex.ru>
2024-07-04 15:31:04 +0300

    card

--- Card.py
+++ Card.py
@@ -1,4 +1,4 @@
-from deck import Card
+from Card import Card
 
 
 class Card:
@@ -15,17 +15,42 @@ class Card:
         return self.number == other.number
 
     def __repr__(self):
-        return f'{self.number}'
+        return str(self.number)
 
-    def save(self):
-        return repr(self)
 
-    @staticmethod
-    def all_cards(umbers=None, numbers=None):
-        if numbers is None:
-            numbers = Card.NUMBERS
-            cards = []
-            for n in numbers:
-                c = Card( n)
-                cards.append(c)
-        return cards
+def __str__(self):
+    return f'{self.number}({self.score()})'
+
+
+def score(self):
+    units = self.number % 10
+    tens = self.number // 10
+    if self.number == 55:
+        return 7
+    if units == tens:  # self.numbers % 11 == 0
+        return 5
+    if units == 0:
+        return 3
+    if units == 5:
+        return 2
+    return 1
+
+
+def save(self):
+    return repr(self)
+
+
+@classmethod
+def load(cls, text: str):
+    return Card(int(text))
+
+
+@staticmethod
+def all_cards(numbers=None):
+    if numbers is None:
+        numbers = Card.NUMBERS
+    cards = []
+    for n in numbers:
+        c = Card(n)
+    cards.append(c)
+    return cards
