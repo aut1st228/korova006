428f9f2d8d4f21c9ec4a1e3a480bd870bee448dd
Andrew <Belka55tt@ysndex.ru>
2024-07-04 15:37:39 +0300

    hand

--- hand.py
+++ hand.py
@@ -1,26 +1,28 @@
-from Card_class import Card
-
-
-class Hand:
+from Card import Card
+class CardList:
     def __init__(self, *args):
-        self.cards = []
-        for i in args:
-            self.cards.append(i.value)
+        self.cards = list(args)
 
     def __repr__(self):
-        cards = []
-        for i in self.cards:
-            cards.append(i)
-        return str(cards)
+        return ' '.join(map(repr, self.cards))   # [3, 64, 27, 5]    '3 64 27 5'
 
     def add_card(self, card):
         self.cards.append(card.value)
 
+    def score(self):
+        summ = 0
+        for card in self.cards:
+            summ += card.score()
+        return summ
+
+    def save(self):
+        return repr(self)
+
+
+class Hand(CardList):
+    def __init__(self, *args):
+        super().__init__(*args)
+
     def remove_card(self, card):
         self.cards.remove(card.value)
 
-    def score(self):
-        summ = 0
-        for i in self.cards:
-            summ += Card(i).point
-        return summ
