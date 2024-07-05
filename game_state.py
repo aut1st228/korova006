c40ac40f36070c0f5b66e36c46c23ab27a8b3b81
Andrew <Belka55tt@ysndex.ru>
2024-07-05 09:47:06 +0300

    game_state

--- game_state.py
+++ game_state.py
@@ -1,9 +1,16 @@
 import json
 from typing import List, Type
-
+from enum import Enum
 from Card import Card
 from deck import Deck
 from player import Player
+
+class GameStage(Enum):
+    PLAY_CARD = 1           # DRAW_CARD, NEXT_TUR
+    PLAY_CARD_AGAIN = 2     # NEXT_TURN
+    NEXT_TURN = 3           # PLAY_CARD
+    END_GAME = 4
+
 
 class GameState:
     def __init__(self, players=list[Player], iplayer: int = 0, deck: Deck = None):
@@ -40,7 +47,32 @@ class GameState:
     @classmethod
     def new_game(cls, players: list[Player]):
         game = cls(players)
+    def run(self) -> Player:
+        """Игра до победы, возвращает игрока-победителя. До этой функции должны быть вызваны или load, или new_game"""
+        stage = GameStage.PLAY_CARD
+        while stage != GameStage.END_GAME:
+            match stage:
+                case GameStage.PLAY_CARD:
+                    stage = self.try_play_card()
+                case GameStage.PLAY_CARD_AGAIN:
+                    stage = self.try_play_card(again=True)
+                case GameStage.NEXT_TURN:
+                    if self.is_win_condition():
+                        stage = GameStage.END_GAME
+                    else:
+                        self.next_player()
+                        stage = GameStage.PLAY_CARD
+        return self.current_player()
 
+    def try_play_card(self, again: bool = False) -> GameStage:
+        """Текущий игрок пытается играть карту с руки. again=True - после того, как уже брал карту из колоды."""
+        p: Player = self.current_player()
+        card = p.choose_card(self.top, [])
+        if card is None:
+            return GameStage.NEXT_TURN
+        p.hand.remove_card(card)
+        self.top = card
+        return GameStage.NEXT_TURN
 
 
 
